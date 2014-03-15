d09c3aba87d6d1cd41048cc818a2b97cdc9624ae
Jedd Ahyoung <jedd.ahyoung@gmail.com>
2014-03-14 23:16:26 -0400

    A little cleanup work.

--- src/memento.js
+++ src/memento.js
@@ -1,30 +1,27 @@
 angular.module('Memento', [])
 	.factory('Memento', function ($log) {
 	    return function (target) {
-    		var _orig = angular.copy(target);
-	        var stack = [];
-	        var cursor = 0;
-
-	        // TODO: add timestamps.
-        	var that = this;
-	        var debug = function () {
-	        	$log.info("CURSOR", that.cursor);
-	        	$log.info("STACK:", that.stack);
-	        };
+    		var _orig = angular.copy(target),
+	            stack = [],
+	            cursor = 0;
 	        
 	        this.canUndo = function () {
         		return stack.length && cursor > 0;
 	        };
+
 	        this.undo = function () {
 	            if (this.canUndo()) {
 	            	cursor--;
-	            	return cursor - 1 >= 0 ? angular.copy(stack[cursor - 1]) : angular.copy(_orig);
+	            	return cursor - 1 >= 0
+	            		? angular.copy(stack[cursor - 1])
+	            		: angular.copy(_orig);
 	            }
 	        };
 
     		this.canRedo = function () {
     			return stack.length && cursor <= stack.length;
     		};
+
 	        this.redo = function () {
 				if (this.canRedo()) {
 	            	cursor++;
@@ -33,16 +30,12 @@ angular.module('Memento', [])
 	        };
 
 	        this.push = function (obj) {
-	        	// Check for equality.
 	            if (angular.equals(stack[cursor - 1], obj)) {
-	            	$log.warn("not pushing");
 	                return false;
 	            }
-	            // Slice array.
 	            if (cursor < stack.length) {
     				stack = stack.slice(0, cursor);
     			}
-	            // Push changes.
 	            cursor++;
 	            return !!stack.push(angular.copy(obj));
 	        };
