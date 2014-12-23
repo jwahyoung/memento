6faf4aef424d389f179535c89e9496d0512b450b
Jedd Ahyoung <jedd.ahyoung@gmail.com>
2014-12-23 14:52:17 -0500

    Update test spec. They were pretty awful before. Sorry.

--- spec/tests.js
+++ spec/tests.js
@@ -1,6 +1,6 @@
 /* Tests for memento.js. */
 
-describe('memento core test suite', function () {
+describe('Memento Core test suite', function () {
 	var memento;
 	var seedObject = {
 		key: 'value',
@@ -15,70 +15,131 @@ describe('memento core test suite', function () {
 		});
 	});
 
-	it('should initialize', function () {
-		expect(memento).toBeTruthy();
+	describe('Initialization', function () {
+		it('should initialize', function () {
+			expect(memento).toBeTruthy();
+		});
+
 		// TODO: Expect memento to return base object.
 	});
 
-	// Check functions.
-	it('should match expected interface', function () { 
-		expect(angular.isFunction(memento.canUndo)).toBe(true);
-		expect(angular.isFunction(memento.canRedo)).toBe(true);
-		expect(angular.isFunction(memento.undo)).toBe(true);
-		expect(angular.isFunction(memento.redo)).toBe(true);
-		expect(angular.isFunction(memento.push)).toBe(true);
+	describe('Core Interface', function () {
+		it('should have canUndo() method', function () { 
+			expect(angular.isFunction(memento.canUndo)).toBe(true);
+		});
+
+		it('should have canRedo() method', function () { 
+			expect(angular.isFunction(memento.canRedo)).toBe(true);
+		});
+
+		it('should have undo() method', function () { 
+			expect(angular.isFunction(memento.undo)).toBe(true);
+		});
+
+		it('should have redo() method', function () { 
+			expect(angular.isFunction(memento.redo)).toBe(true);
+		});
+
+		it('should have push() method', function () { 
+			expect(angular.isFunction(memento.push)).toBe(true);
+		});
+
+		it('should have revert() method', function () { 
+			expect(angular.isFunction(memento.revert)).toBe(true);
+		});
 	});
 
-	it('should push() a changed object', function () {
-		expect(memento.push({ key: 'value', num: 2 })).toBe(true);
-	});
+	describe('End-to-End functionality', function () {
 
-	it('should not push() an unchanged object', function () {
-		expect(memento.push({ key: 'value', num: 2 })).toBe(true);
-		expect(memento.push({ key: 'value', num: 2 })).toBe(false);
+		// TODO: This is not a real unit test; it's integration. Should be
+		//       broken out somehow.
 
-		// TODO: Deep copy tests.
-	});
+		describe('canUndo()', function () {
+			it('should return false if stack is empty', function () {
+				expect(memento.canUndo()).toBe(false);
+			});
 
-	it('should return original object on undo()', function () {
-		memento.push({ key: 'value', num: 2 });
-		expect(memento.undo()).toEqual(seedObject);
+			it('should return false if at beginning of nonempty stack', function () {
+				memento.push({ key: 'value', num: 2 });
+				memento.undo();
+				expect(memento.canUndo()).toBe(false);
+			});
 
-		memento.push({ key: 'value', num: 2 });
-		memento.push({ key: 'value', num: 3 });
-		expect(memento.undo()).toEqual({ key: 'value', num: 2 });
-	});
+			it('should return true if not at beginning of nonempty stack', function () {
+				memento.push({ key: 'value', num: 2 });
+				expect(memento.canUndo()).toBe(true);
 
-	it('should return modified object on redo()', function () {
-		memento.push({ key: 'value', num: 2 });
-		memento.undo();
-		expect(memento.redo()).toEqual({ key: 'value', num: 2 });
-	});
+				memento.push({ key: 'value', num: 3 });
+				memento.push({ key: 'value', num: 4 });
+				expect(memento.canUndo()).toBe(true);
+			});
+		});
 
-	it('should not undo() if at beginning of stack', function () {
-		expect(memento.undo()).toBeUndefined();
-		memento.push({ key: 'value', num: 2 });
-		memento.undo();
-		expect(memento.undo()).toBeUndefined();
-	});
+		describe('canRedo()', function () {
 
-	it('should not redo() if at end of stack', function () {
-		expect(memento.redo()).toBeUndefined();
-		memento.push({ key: 'value', num: 2 });
-		memento.undo();
-		memento.redo();
-		expect(memento.redo()).toBeUndefined();
+		});
+
+		describe('undo()', function () {
+			it('should not undo() if at beginning of stack', function () {
+				expect(memento.undo()).toBeUndefined();
+				memento.push({ key: 'value', num: 2 });
+				memento.undo();
+				expect(memento.undo()).toBeUndefined();
+			});
+
+			it('should return original object on undo()', function () {
+				memento.push({ key: 'value', num: 2 });
+				expect(memento.undo()).toEqual(seedObject);
+
+				memento.push({ key: 'value', num: 2 });
+				memento.push({ key: 'value', num: 3 });
+				expect(memento.undo()).toEqual({ key: 'value', num: 2 });
+			});
+		});
+
+		describe('redo()', function () {
+			it('should not redo() if at end of stack', function () {
+				expect(memento.redo()).toBeUndefined();
+				memento.push({ key: 'value', num: 2 });
+				memento.undo();
+				memento.redo();
+				expect(memento.redo()).toBeUndefined();
+			});
+
+			it('should return modified object on redo()', function () {
+				memento.push({ key: 'value', num: 2 });
+				memento.undo();
+				expect(memento.redo()).toEqual({ key: 'value', num: 2 });
+			});
+		});
+
+		describe('push()', function () {
+			it('should push() a changed object', function () {
+				expect(memento.push({ key: 'value', num: 2 })).toBe(true);
+			});
+
+			it('should not push() an unchanged object', function () {
+				expect(memento.push({ key: 'value', num: 2 })).toBe(true);
+				expect(memento.push({ key: 'value', num: 2 })).toBe(false);
+
+				// TODO: Deep copy tests.
+			});
+		});
+
+		describe('revert()', function () {
+			it('should return seed object on revert()', function () {
+				memento.push({ key: 'value', num: 2 });
+				memento.push({ key: 'value', num: 3 });
+				memento.push({ key: 'value', num: 4 });
+				memento.push({ key: 'value', num: 5 });
+				expect(memento.revert()).toEqual(seedObject);
+			});
+		});
+
+		//describe('clear()', function () {
+		//	it('should clear the stack on clear()', function () {
+		//		this.fail(Error('Test not implemented'));
+		//	});
+		//});
 	});
-	
-	it('should return seed object on revert()', function () {
-		memento.push({ key: 'value', num: 2 });
-		memento.push({ key: 'value', num: 3 });
-		memento.push({ key: 'value', num: 4 });
-		memento.push({ key: 'value', num: 5 });
-		expect(memento.revert()).toEqual(seedObject);
-	});
- 
- 	// it('should clear the stack on clear()', function () {
- 	// 	this.fail(Error('Test not implemented'));
-	// });
 });
